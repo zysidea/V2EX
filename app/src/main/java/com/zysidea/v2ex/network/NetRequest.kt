feb14cb98c1f988217868e96a3ecc105bb89b5f2
zysidea <zysidea@gmail.com>
2017-06-21 14:43:06 +0800

    添加网络请求

---
+++ app/src/main/java/com/zysidea/v2ex/network/NetRequest.kt
@@ -0,0 +1,51 @@
+package com.zysidea.v2ex.network
+
+import android.content.Context
+
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+import okhttp3.OkHttpClient
+
+/**
+ * Created by zys on 17-6-21.
+ */
+
+class NetRequest private constructor(private val mContext: Context) {
+
+    init {
+        mRequestQueue = requestQueue
+    }
+
+
+    companion object {
+
+        private var mRequestQueue: RequestQueue? = null
+        private var mNetRequest: NetRequest? = null
+
+        @Synchronized fun getInstance(context: Context): NetRequest {
+            if (mNetRequest == null) {
+                mNetRequest = NetRequest(context)
+            }
+            return mNetRequest!!
+        }
+    }
+
+    val requestQueue: RequestQueue
+        get() {
+            if (mRequestQueue == null) {
+                synchronized(NetRequest::class.java) {
+                    if (mRequestQueue == null) {
+                        mRequestQueue = Volley.newRequestQueue(mContext.applicationContext, OkHttpStack(OkHttpClient()))
+                    }
+                }
+            }
+            return mRequestQueue!!
+        }
+
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
+    }
+
+}
