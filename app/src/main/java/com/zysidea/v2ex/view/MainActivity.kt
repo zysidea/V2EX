0dac7b7b02813e297381d0a28549e83334117754
zysidea <zysidea@gmail.com>
2017-07-04 15:12:06 +0800

    修改单元测试类

--- app/src/main/java/com/zysidea/v2ex/view/MainActivity.kt
+++ app/src/main/java/com/zysidea/v2ex/view/MainActivity.kt
@@ -5,31 +5,18 @@
 package com.zysidea.v2ex.view
 
 import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
+import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import com.zysidea.v2ex.R
-import com.zysidea.v2ex.util.UnitUtils
-import com.zysidea.v2ex.view.Node.NodeFragment
-import com.zysidea.v2ex.view.home.HomeFragment
-import com.zysidea.v2ex.view.home.HomePageItemFragment
-import android.opengl.ETC1.getHeight
-import android.icu.lang.UCharacter.GraphemeClusterBreak.V
-import android.R.attr.translationY
-import android.os.Build
-import android.icu.lang.UCharacter.GraphemeClusterBreak.V
-import android.support.v4.view.ViewCompat.getTranslationY
-import android.view.ViewGroup
-import android.R.string.cancel
-import android.icu.lang.UCharacter.GraphemeClusterBreak.V
-import android.support.v4.view.ViewPropertyAnimatorUpdateListener
 import android.support.v4.view.ViewCompat
 import android.support.v4.view.ViewPropertyAnimatorCompat
 import android.support.v4.view.animation.LinearOutSlowInInterpolator
-import android.view.ViewPropertyAnimator
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.zysidea.v2ex.R
+import com.zysidea.v2ex.view.node.NodeFragment
+import com.zysidea.v2ex.view.home.HomeFragment
+import com.zysidea.v2ex.view.home.HomePageItemFragment
 
 
 class MainActivity : AppCompatActivity(), HomePageItemFragment.OnRecyclerViewScrollListener {
@@ -122,16 +109,20 @@ class MainActivity : AppCompatActivity(), HomePageItemFragment.OnRecyclerViewScr
         }
     }
 
-    private fun ensureOrCancelAnimator( withAnimation: Boolean) {
+    private fun ensureOrCancelAnimator(withAnimation: Boolean) {
         if (translationAnimator == null) {
             translationAnimator = ViewCompat.animate(navigation!!)
             translationAnimator!!.setDuration(if (withAnimation) 300 else 0)
-            translationAnimator!!.setInterpolator(LinearOutSlowInInterpolator())
+            translationAnimator!!.interpolator=LinearOutSlowInInterpolator()
         } else {
             translationAnimator!!.setDuration(if (withAnimation) 300 else 0)
             translationAnimator!!.cancel()
         }
     }
+
+    /**
+     * 低于4.4
+     */
     private fun ensureOrCancelObjectAnimation(offset: Int, withAnimation: Boolean) {
 
         if (translationObjectAnimator != null) {
@@ -139,7 +130,7 @@ class MainActivity : AppCompatActivity(), HomePageItemFragment.OnRecyclerViewScr
         }
         translationObjectAnimator = ObjectAnimator.ofFloat(navigation,View.TRANSLATION_Y, offset.toFloat())
         translationObjectAnimator!!.setDuration(if (withAnimation) 300 else 0)
-        translationObjectAnimator!!.setInterpolator(LinearOutSlowInInterpolator())
+        translationObjectAnimator!!.interpolator=LinearOutSlowInInterpolator()
     }
 
 }
